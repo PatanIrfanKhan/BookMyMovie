af56ea66992d9abfd76f288d656d84293a815058
Patan Irfan Khan <76727330+PatanIrfanKhan@users.noreply.github.com>
2026-01-04 23:14:57 +0530

    all changes

--- src/main/java/com/example/BookMyMovie/BookMyMovieApplication.java
+++ src/main/java/com/example/BookMyMovie/BookMyMovieApplication.java
@@ -3,23 +3,32 @@ package com.example.BookMyMovie;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookMyMovieApplication {
 
+	@Value("${spring.security.auth2.client.registration.google.client-id}")
+	private String client_id;
+
+	@Value("${spring.security.auth2.client.registration.google.client-secret}")
+	private String client_secret;
+
 	Logger logger= LoggerFactory.getLogger(BookMyMovieApplication.class);
 
 	public static void main(String[] args)
 	{
 		SpringApplication.run(BookMyMovieApplication.class, args);
+
 	}
 
 //	private static final Logger log = LogManager.getLogger(BookMyMovieApplication.class);
 
 	@PostConstruct
 	public void testLog() {
+		logger.info("cleinrt id: {} and clent-secret: {}",client_id,client_secret);
 		logger.info("BOOKMYMOVIE APPLICATION LOG TEST");
 	}
 
